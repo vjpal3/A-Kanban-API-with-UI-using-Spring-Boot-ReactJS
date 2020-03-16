d0b0cfdce6a414b9b2ecdb264698227f8febf2a5
vjpal3 <vrishalipal@yahoo.com>
2020-03-16 18:44:22 -0400

    Setup Project Object and Project Repository

---
+++ ppmtool/src/main/java/com/vjpal/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.vjpal.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vjpal.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+	
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+}
