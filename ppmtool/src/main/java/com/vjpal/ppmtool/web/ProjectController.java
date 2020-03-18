ec5fac4a7d47902e6a5a2c3e902c086252db017d
vjpal3 <vrishalipal@yahoo.com>
2020-03-18 18:05:08 -0400

    Refactor controller, move error checking logic in separate service

--- ppmtool/src/main/java/com/vjpal/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/vjpal/ppmtool/web/ProjectController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vjpal.ppmtool.domain.Project;
+import com.vjpal.ppmtool.services.MapValidationErrorService;
 import com.vjpal.ppmtool.services.ProjectService;
 
 @RestController
@@ -26,18 +27,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+		
 	@PostMapping("") 
 	public ResponseEntity<?> CreateNewProject(@Valid @RequestBody Project project, BindingResult result) {
 		
-		if(result.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<String, String>();
-			for(FieldError error: result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			};
-			
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
-				
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if (errorMap != null) return errorMap;
+						
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}
