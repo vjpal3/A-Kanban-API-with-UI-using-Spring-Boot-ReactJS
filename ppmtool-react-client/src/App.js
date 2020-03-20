56ef8d23c18bbe4be368f0ea91e31bd1be384020
vjpal3 <vrishalipal@yahoo.com>
2020-03-20 17:50:28 -0400

    Add Dashboard, Header and ProjectItem components

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
