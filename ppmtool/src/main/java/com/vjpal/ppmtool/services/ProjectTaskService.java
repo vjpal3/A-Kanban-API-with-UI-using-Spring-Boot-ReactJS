003001dbc86cc0a2ffb9ebe2519f7bb6dc2fa2f8
vjpal3 <vrishalipal@yahoo.com>
2020-03-28 22:32:24 -0400

    Get Project Backlog only

--- ppmtool/src/main/java/com/vjpal/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/com/vjpal/ppmtool/services/ProjectTaskService.java
@@ -1,5 +1,7 @@
 package com.vjpal.ppmtool.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -50,6 +52,11 @@ public class ProjectTaskService {
 		}
 		
 		return projectTaskRepository.save(projectTask);
+	}
+
+	public Iterable<ProjectTask> findBacklogById(String id) {
+		
+		return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
 	}
 
 }
