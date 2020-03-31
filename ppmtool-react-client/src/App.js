3d3683f650b2a706c888fa5b4e0bb72de48d47b9
vjpal3 <vrishalipal@yahoo.com>
2020-03-30 22:35:33 -0400

    Add routes to ProjectBoard and AddProjectTask

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -8,8 +8,10 @@ import Header from './components/Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
+import ProjectBoard from './components/ProjectBoard/ProjectBoard';
 
 import store from './store';
+import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
 
 class App extends Component {
   render() {
@@ -21,6 +23,12 @@ class App extends Component {
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
+            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+            <Route
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            />
           </div>
         </Router>
       </Provider>
