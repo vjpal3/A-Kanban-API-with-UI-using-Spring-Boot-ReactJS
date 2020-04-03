b685016cd213146e36bb31024c63652b6234ff2c
vjpal3 <vrishalipal@yahoo.com>
2020-04-03 16:12:47 -0400

    Delete Project Task

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from './types';
+import {
+  GET_ERRORS,
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK
+} from './types';
 
 export const addProjectTask = (
   backlog_id,
@@ -72,6 +77,20 @@ export const updateProjectTask = (
     dispatch({
       type: GET_ERRORS,
       payload: err.response.data
+    });
+  }
+};
+
+export const deleteProjectTask = (backlog_id, pt_id) => async dispatch => {
+  if (
+    window.confirm(
+      `You are deleting the project task ${pt_id}, this action csannot be undone.`
+    )
+  ) {
+    await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`);
+    dispatch({
+      type: DELETE_PROJECT_TASK,
+      payload: pt_id
     });
   }
 };
--- ppmtool-react-client/src/reducers/backlogReducer.js
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -25,7 +25,10 @@ export default function(state = initialState, action) {
 
     case DELETE_PROJECT_TASK:
       return {
-        ...state
+        ...state,
+        project_tasks: state.project_tasks.filter(
+          project_task => project_task.projectSequence !== action.payload
+        )
       };
 
     default:
