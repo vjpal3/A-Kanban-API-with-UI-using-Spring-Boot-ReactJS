3b0423826e783fa063906e9e9e5bc2c3698f2cf7
vjpal3 <vrishalipal@yahoo.com>
2020-04-08 12:24:24 -0400

    Custom JWT filter to authenticate tokens

--- ppmtool/src/main/java/com/vjpal/ppmtool/security/JwtTokenProvider.java
+++ ppmtool/src/main/java/com/vjpal/ppmtool/security/JwtTokenProvider.java
@@ -9,8 +9,13 @@ import org.springframework.stereotype.Component;
 
 import com.vjpal.ppmtool.domain.User;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 import static com.vjpal.ppmtool.security.SecurityConstants.EXPIRATION_TIME;  
 import static com.vjpal.ppmtool.security.SecurityConstants.SECRET;  
@@ -44,7 +49,29 @@ public class JwtTokenProvider {
 	}
 	
 	//Validate token
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+			return true;
+			
+		} catch (SignatureException ex) {
+			System.out.println("Invalid JWT Signature");
+		} catch (MalformedJwtException ex) {
+			System.out.println("Invalid JWT Token");
+		} catch (ExpiredJwtException ex) {
+			System.out.println("Expired JWT token");
+		} catch (UnsupportedJwtException ex) {
+			System.out.println("Unsupported JWT token");
+		} catch (IllegalArgumentException ex) {
+			System.out.println("JWT claims string is empty");
+		}
+		return false;
+	}
 	
 	//Get userId from token
-
+	public Long getUserIdFromJWT(String token) {
+		Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+		String id = (String)claims.get("id");
+		return Long.parseLong(id);
+	}
 }
